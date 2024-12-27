ae5d0bda2f2a03c9bd79730f11c71c0fd5c283ff
Matias Silva <maybeaferret@gmail.com>
2024-12-27 15:53:19 -0300

    Reestructuracion de 'verPaquetes' y correccion de errores

--- postbuild.js
+++ postbuild.js
@@ -5,10 +5,10 @@ const commitMessage = process.argv[2] || "Actualización después de build";
 
 try {
     // Ejecuta los comandos necesarios
+    execSync('firebase deploy --only "hosting"', { stdio: "inherit" });
     execSync("git add .", { stdio: "inherit" });
     execSync(`git commit -m "${commitMessage}"`, { stdio: "inherit" });
     execSync("git push", { stdio: "inherit" });
-    execSync('firebase deploy --only "hosting"', { stdio: "inherit" });
 } catch (error) {
     console.error("Error durante el postbuild:", error.message);
     process.exit(1);
