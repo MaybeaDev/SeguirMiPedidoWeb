f548210c5714b255e65c6634e0290ec0adcbb92c
Matias Silva <maybeaferret@gmail.com>
2025-01-07 12:45:34 -0300

    AAAAAAAAAA MI BOTON

--- src/components/Layout/NavBar/Navbar.tsx
+++ src/components/Layout/NavBar/Navbar.tsx
@@ -5,9 +5,9 @@ import LinkButton from "../../UI/LinkButton/LinkButton";
 import classes from "./NavBar.module.css"
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../../firebaseConfig"
-import { db } from "../../../firebaseConfig"
-import { collection, doc, getDocs, query, where, writeBatch } from "firebase/firestore";
-import Button from "../../UI/Button/Button";
+// import { db } from "../../../firebaseConfig"
+// import { collection, doc, getDocs, query, where, writeBatch } from "firebase/firestore";
+// import Button from "../../UI/Button/Button";
 const Navbar = () => {
     const [user, setUser] = useState<boolean>(false);
     useEffect(() => {
@@ -27,27 +27,27 @@ const Navbar = () => {
         return () => unsubscribe();
     }, []);
 
-    const handleFunction = async () => {
-        const batch = writeBatch(db)
+    // const handleFunction = async () => {
+    //     const batch = writeBatch(db)
         
-        const q = query(collection(db, "Paquetes"), where("estado", "==", 3), where("ruta", "==", ""))
-        const paquetes = (await getDocs(q)).docs.map(d => {
-            return {
-                id: d.id,
-                ruta: d.data().ruta,
-            }
-        })
-        console.log(paquetes)
-        paquetes.forEach(p => {
-            batch.update(doc(db, "Paquetes", p.id), { ruta: "Sistema", rutaAlias:"Sistema", transportistaNombre:"Sistema" })
-        })
-        console.log("Guardando...")
-        batch.commit().then(() => {
-            console.log("Batch de actualización exitosa");
-        }).catch((error) => {
-            console.error("Error al actualizar batch: ", error);
-        });
-    }
+    //     const q = query(collection(db, "Paquetes"), where("estado", "==", 3), where("ruta", "==", ""))
+    //     const paquetes = (await getDocs(q)).docs.map(d => {
+    //         return {
+    //             id: d.id,
+    //             ruta: d.data().ruta,
+    //         }
+    //     })
+    //     console.log(paquetes)
+    //     paquetes.forEach(p => {
+    //         batch.update(doc(db, "Paquetes", p.id), { ruta: "Sistema", rutaAlias:"Sistema", transportistaNombre:"Sistema" })
+    //     })
+    //     console.log("Guardando...")
+    //     batch.commit().then(() => {
+    //         console.log("Batch de actualización exitosa");
+    //     }).catch((error) => {
+    //         console.error("Error al actualizar batch: ", error);
+    //     });
+    // }
 
 
     return (
@@ -58,7 +58,7 @@ const Navbar = () => {
                 )}
             </div>
             <div className={classes.center}>
-                <Button onClick={handleFunction}>Boton solo para matias</Button>
+                {/* <Button onClick={handleFunction}>Boton solo para matias</Button> */}
             </div>
             <div className={classes.right}>
                 {user ? (
