0a583ffd9678416bdf549ef8ffed78d637df0ff6
Matias Silva <maybeaferret@gmail.com>
2025-04-15 20:29:21 -0400

    Actualización después de build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { visualizer } from 'rollup-plugin-visualizer';
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
   server: {
     host: '0.0.0.0',
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ['firebase/app', 'firebase/firestore'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // opcional: sube el límite si ya sabés que todo está bien
+  },
+});
